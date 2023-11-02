c65fe2fe5b2bbce6b281d904734260fe12bfd44c
EA-Gadgeter <emiacevedoposo@gmail.com>
2023-11-02 16:30:48 -0600

    Fixed sequelize for change URI from prod to dev

--- config/config.js
+++ config/config.js
@@ -10,12 +10,14 @@ dotenv.config();
 const appConfig = {
   env: process.env.NODE_ENV ?? "dev",
   port: process.env.PORT ?? 3000,
+  idProd: process.env.NODE_ENV === "production",
   db: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     name: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    url: process.env.DATABASE_URL
   }
 };
 
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -7,9 +7,16 @@ const setupModels = require("../db/models/index.js");
 // de datos, que es por un ORM, en el caso de sequelize que
 // es el que estamos usando, ya se encarga de generar
 // el pool de conexiones
-const USER = encodeURIComponent(appConfig.db.user);
-const PASSWORD = encodeURIComponent(appConfig.db.password);
-const URI = `postgres://${USER}:${PASSWORD}@${appConfig.db.host}:${appConfig.db.port}/${appConfig.db.name}`;
+
+let URI;
+
+if (appConfig.idProd) {
+    URI = appConfig.db.url;
+} else {
+    const USER = encodeURIComponent(appConfig.db.user);
+    const PASSWORD = encodeURIComponent(appConfig.db.password);
+    URI = `postgres://${USER}:${PASSWORD}@${appConfig.db.host}:${appConfig.db.port}/${appConfig.db.name}`;
+}
 
 const sequelize = new Sequelize(URI, {
     dialect: "postgres",
